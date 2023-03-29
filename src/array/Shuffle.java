158b811d5328d16d0c31c79b375e377f4709b12c
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-29 15:59:18 +0100

    shuffle

--- src/array/Shuffle.java
+++ src/array/Shuffle.java
@@ -1,19 +1,20 @@
 package array;
 
+import java.util.Arrays;
+
 public class Shuffle {
     public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,6,7,8};
+        System.out.println(Arrays.toString(shuffle(nums, 4)));
 
     }
     public static int[] shuffle(int[] nums, int n){
-        int[] arr = new int[2*n];
-
-        for (int i = 0; i < n; i++) {
-
+        int size = nums.length / 2;
+        int[] result = new int[n*2];
+        for (int i = 0; i < size; i++) {
+            result[2*i] = nums[i];
+            result[2*i+1] = nums[i+size];
         }
-        for (int i = n; i <= nums.length; i++) {
-
-        }
-
-        return arr;
+        return result;
     }
 }

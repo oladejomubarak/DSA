d4958bc5a784fd116cd9c87affa048b522e63634
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-29 18:16:59 +0100

    monotonic

--- src/array/Monotonic.java
+++ src/array/Monotonic.java
@@ -2,7 +2,26 @@ package array;
 
 public class Monotonic {
     public static void main(String[] args) {
+        int[] arr = {1,2,2,3};
+        int[] arr1 = {6,5,4,4};
+        int[] arr2 = {1,3,2};
+
+
 
     }
-
+public static boolean isMonotonic(int[] nums){
+    boolean result = true;
+    int i = 0;
+    int j = i+ 1;
+    if(nums[0] > nums[1]){
+        for (i = 0; i < nums.length; i++){
+            if(nums[i] < nums[j])  result = false;
+        }
+    } else {
+        for (i = 0; i < nums.length; i++){
+            if(nums[i] > nums[j])  result = false;
+        }
+    }
+        return result;
+}
 }

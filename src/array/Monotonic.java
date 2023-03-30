010ced27bfc7f1d3ab6d7e59fbe07cf37612341e
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-30 10:56:19 +0100

    monotonic

--- src/array/Monotonic.java
+++ src/array/Monotonic.java
@@ -1,27 +1,42 @@
 package array;
 
+import java.util.Arrays;
+
 public class Monotonic {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3};
         int[] arr1 = {6,5,4,4};
         int[] arr2 = {1,3,2};
+        int[] arr3= {1,3,2,4};
 
-
-
+        System.out.println(isMonotonic(arr));
+        System.out.println(isMonotonic(arr1));
+        System.out.println(isMonotonic((arr2)));
+        System.out.println(isMonotonic(arr3));
     }
+
 public static boolean isMonotonic(int[] nums){
     boolean result = true;
-    int i = 0;
-    int j = i+ 1;
     if(nums[0] > nums[1]){
-        for (i = 0; i < nums.length; i++){
-            if(nums[i] < nums[j])  result = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                result = false;
+                break;
+            }
         }
-    } else {
-        for (i = 0; i < nums.length; i++){
-            if(nums[i] > nums[j])  result = false;
+        }
+    }
+    if (nums[0] < nums[1]){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                result = false;
+                break;
+            }
+        }
         }
     }
         return result;
-}
+ }
 }

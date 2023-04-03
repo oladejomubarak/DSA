620912e4c58c3aa03eb5ad484d9806c4c83421aa
oladejomubarak <oladejomubarakade@gmail.com>
2023-04-03 05:53:39 +0100

    findDuplicates

--- src/array/FindAllDuplicates.java
+++ src/array/FindAllDuplicates.java
@@ -7,16 +7,18 @@ import java.util.Map;
 
 public class FindAllDuplicates {
     public static void main(String[] args) {
+        int [] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(nums));
 
     }
-    public List<Integer> findDuplicates(int[] nums){
+    public static List<Integer> findDuplicates(int[] nums){
         List<Integer> duplicates = new ArrayList<>();
         Map<Integer,Integer> map = new HashMap<>();
 
         for (int num : nums) {
             if (map.containsKey(num)) {
-                int count = map.get(num);
-                map.put(num, count + 1);
+                int value = map.get(num);
+                map.put(num, value + 1);
             } else {
                 map.put(num, 1);
             }

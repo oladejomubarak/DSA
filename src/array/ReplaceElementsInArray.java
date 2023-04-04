f09a3697d0c42d83d0b486d62c474fe6ec81aab7
oladejomubarak <oladejomubarakade@gmail.com>
2023-04-04 09:01:54 +0100

    replaceElementInArray

--- src/array/ReplaceElementsInArray.java
+++ src/array/ReplaceElementsInArray.java
@@ -1,10 +1,44 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ReplaceElementsInArray {
     public static void main(String[] args) {
-
+        int[] arr1 = {1,2,4,6};
+        int[][] arr2 = {{1,3}, {4,7}, {6,1}};
+        System.out.println(Arrays.toString(replace(arr1, arr2)));
     }
     public static int[] replace(int[] nums, int[][] operations){
-return null;
+        List<Integer> list = new ArrayList<>();
+        for (var i: nums
+             ) { list.add(i);
+        }
+//        for (int[] i: operations
+//             ) {
+//            for (int j: i
+//                 ) {if (list.contains(i)) {
+//                     list.remove(i);
+//                     list.add(j);
+//                 }
+//
+//            }
+//
+//        }
+        for (int i = 0; i < operations.length; i++) {
+            for (int j = 0; j < i; j++) {
+
+                if (list.contains(operations[i][j])) {
+                    list.remove(operations[i][0]);
+                    list.add(operations[i][1]);
+                }
+            }
+        }
+        int[] num2 = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            num2[i] = list.get(i);
+        }
+    return num2;
     }
 }

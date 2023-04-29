655d519438891497e5050c3422d845a60bd669f9
oladejomubarak <oladejomubarakade@gmail.com>
2023-04-29 13:49:20 +0100

    search numbers of characters in a text

---
+++ src/dietelChap14/SearchingStrings.java
@@ -0,0 +1,11 @@
+package dietelChap14;
+
+public class SearchingStrings {
+    public static void main(String[] args) {
+
+    }
+    public static int searchNumberOfCharInAString(String text, char character){
+
+        return 0;
+    }
+}

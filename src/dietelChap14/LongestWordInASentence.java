cdb709e0c43d8c8600aa553f8789d43b8d5a036c
oladejomubarak <oladejomubarakade@gmail.com>
2023-04-29 13:21:34 +0100

    longest word in a sentence

--- src/dietelChap14/LongestWordInASentence.java
+++ src/dietelChap14/LongestWordInASentence.java
@@ -1,16 +1,17 @@
 package dietelChap14;
 
+import java.util.Arrays;
+
 public class LongestWordInASentence {
     public static void main(String[] args) {
-        String sentence = "I am a nice guy truly";
+        String sentence = "I am a truly guy nice ";
         System.out.println(findLongestWord(sentence));
 
     }
     public static String findLongestWord(String sentence){
         var splitWord = sentence.split("\\s+");
-        String longestWord = splitWord[0];
-        int maxLength = longestWord.length();
-        for (String word: splitWord) { if (word.length() > maxLength) longestWord = word;}
+        String longestWord = "";
+        for (String word: splitWord) { if (word.length() > longestWord.length()) longestWord = word;}
         return longestWord;
     }
 }
